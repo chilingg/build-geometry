e7982553c399d5dea67a2142f2214fc5b1170382
chilingg <chilingg@qq.com>
2022-12-16 17:37:14 +0800

    The game system moves to the backend

--- build-geometry/src/main.rs
+++ build-geometry/src/main.rs
@@ -1,24 +1,4 @@
-use backend::app::App;
-use winit::{
-    event_loop::EventLoop,
-    window::WindowBuilder,
-};
-
-mod game_system;
-use game_system::GameSystem;
 
 fn main() {
-    backend::app::init();
-
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        // .with_maximized(true)
-        .with_inner_size( winit::dpi::PhysicalSize {
-                width: 800,
-                height: 800,
-        })
-        .build(&event_loop)
-        .unwrap();
-
-    App::new(event_loop, window).run(GameSystem::new());
+    
 }
